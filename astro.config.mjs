0cb7b914f2aa5b45d14584bfa48540f426e84c50
byronjvh <byronjvh02@gmail.com>
2025-08-10 15:49:25 -0600

    Deleting pdf dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,12 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: []
 });
